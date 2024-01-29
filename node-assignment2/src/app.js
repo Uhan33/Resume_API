0760b9cf9e09d2b5dba8de0fc40e804514b5b5da
Uhan33 <jazzcafe12@kangwon.ac.kr>
2024-01-29 21:28:38 +0900

    개인과제 기본 틀 부분 작성

---
+++ node-assignment2/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import UsersRouter from './routes/users.router.js';
+import ResumesRouter from './routes/resumes.router.js';
+import logMiddleware from './middlewares/log.middleware.js';
+import errorHandlingMiddleware from './middlewares/error-handling.middleware.js';
+// import expressSession from 'express-session';
+import expressMySQLSession from 'express-mysql-session';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = 3018;
+
+app.use(logMiddleware);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api', [UsersRouter, ResumesRouter]);
+app.use(errorHandlingMiddleware);
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
+});
